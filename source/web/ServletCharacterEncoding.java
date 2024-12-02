a7e1b9a8ff5b5fa329930fd7d606f0e746e4dd20
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-12-02 01:24:03 -0300

    feat: ServletCharacterEncoding.java

---
+++ source/web/ServletCharacterEncoding.java
@@ -0,0 +1,26 @@
+package com.assodepicche.web;
+
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter(
+    filterName = "ServletCharacterEncoding",
+    urlPatterns = {"/*"})
+public final class ServletCharacterEncoding implements Filter {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+    chain.doFilter(request, response);
+  }
+}
