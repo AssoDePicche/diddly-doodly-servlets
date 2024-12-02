2e1fd49672abe565904d290102d5c2330d2d1614
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-12-02 01:18:34 -0300

    feat: Payload.java

---
+++ source/web/Payload.java
@@ -0,0 +1,44 @@
+package com.assodepicche.web;
+
+import com.assodepicche.shared.Json;
+
+import java.time.ZonedDateTime;
+
+import java.util.Date;
+
+public final class Payload<T> {
+  public int code;
+
+  public String message;
+
+  public final T payload;
+
+  public final Date date;
+
+  public Payload(int code, String message, T payload, Date date) {
+    this.code = code;
+
+    this.message = message;
+
+    this.payload = payload;
+
+    this.date = date;
+  }
+
+  public Payload(int code, String message, T payload) {
+    this(code, message, payload, Date.from(ZonedDateTime.now().toInstant()));
+  }
+
+  public Payload(Status status, String message, T payload) {
+    this(status.code, message, payload);
+  }
+
+  public Payload(Status status, T payload) {
+    this(status.code, status.message, payload);
+  }
+
+  @Override
+  public String toString() {
+    return Json.from(this);
+  }
+}
