f0c353406a5351c45b1f5b06b16606f19177f2a1
AssoDePicche <samuelprado730@gmail.com>
2024-12-17 16:30:36 -0300

    refactor: add right origin + fix annotation

--- source/servlet/filter/CorsFilter.java
+++ source/servlet/filter/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.assodepicche.servlet.filter;
 
+import com.assodepicche.servlet.HttpStatus;
+
 import java.io.IOException;
 
 import jakarta.servlet.Filter;
@@ -10,21 +12,23 @@ import jakarta.servlet.ServletResponse;
 
 import jakarta.servlet.annotation.WebFilter;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebFilter(
-    filterName = "CorsFilter",
-    urlPatterns = {"/*"})
+@WebFilter("/*")
 public final class CorsFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+    ((HttpServletResponse) response)
+        .setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
 
     ((HttpServletResponse) response)
-        .addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+        .setHeader("Access-Control-Allow-Methods", "GET, DELETE, OPTIONS, HEAD, PUT, POST");
 
-    ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "*");
+    ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "*");
+
+    ((HttpServletResponse) response).setHeader("Access-Control-Allow-Credentials", "true");
 
     chain.doFilter(request, response);
   }
