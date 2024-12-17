bb9db93fffc936272610aac70422822a396b2c4b
AssoDePicche <samuelprado730@gmail.com>
2024-12-16 21:34:23 -0300

    refactor: imports + JsonObject wrapper

--- source/shared/Json.java
+++ source/shared/Json.java
@@ -2,14 +2,28 @@ package com.assodepicche.shared;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.time.LocalDateTime;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public final class Json {
-  private static Gson gson;
+  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-  static {
-    gson = new GsonBuilder().setPrettyPrinting().create();
+  private JsonObject jsonObject;
+
+  private Json(JsonObject jsonObject) {
+    this.jsonObject = jsonObject;
+  }
+
+  public static Json from(BufferedReader reader) {
+    return new Json(JsonParser.parseReader(reader).getAsJsonObject());
+  }
+
+  public JsonElement get(String key) {
+    return this.jsonObject.get(key);
   }
 
   public static <T> T from(String json, Class<T> classOfT) {
