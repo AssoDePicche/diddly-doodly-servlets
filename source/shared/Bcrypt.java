47f5fa663212a1a9180d22cd7dec0a67128c76d2
AssoDePicche <samuelprado730@gmail.com>
2024-12-16 21:33:14 -0300

    feat: Bcrypt.java

---
+++ source/shared/Bcrypt.java
@@ -0,0 +1,15 @@
+package com.assodepicche.shared;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public final class Bcrypt implements Cipher {
+  @Override
+  public String encrypt(String value) {
+    return BCrypt.hashpw(value, BCrypt.gensalt());
+  }
+
+  @Override
+  public boolean check(String cipher, String value) {
+    return BCrypt.checkpw(cipher, value);
+  }
+}
