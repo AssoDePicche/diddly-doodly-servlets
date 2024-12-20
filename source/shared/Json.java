662b03541a8c2d92bbd09a39b4ed286d2386bf44
AssoDePicche <samuelprado730@gmail.com>
2024-12-20 13:23:38 -0300

    feat: LocaDate adapter

--- source/shared/Json.java
+++ source/shared/Json.java
@@ -5,12 +5,31 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.lang.reflect.Type;
+
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+
+class LocalDateAdapter implements JsonSerializer<LocalDate> {
+  public JsonElement serialize(LocalDate date, Type type, JsonSerializationContext context) {
+    return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+  }
+}
+
 public final class Json {
-  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  private static Gson gson =
+      new GsonBuilder()
+          .setPrettyPrinting()
+          .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+          .create();
 
   private JsonObject jsonObject;
 
