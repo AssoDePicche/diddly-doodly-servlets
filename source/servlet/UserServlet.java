2686df3ae5e2ef008ae5e81f23e8d281ec4b48ba
AssoDePicche <samuelprado730@gmail.com>
2024-12-21 06:11:29 -0300

    fix some things

--- source/servlet/UserServlet.java
+++ source/servlet/UserServlet.java
@@ -15,13 +15,19 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.UUID;
 
+import java.util.regex.Pattern;
+
 import persistence.UserDAO;
 
 import shared.Bcrypt;
+import shared.Cipher;
 
 @SuppressWarnings({"serial"})
 @WebServlet("/users")
 public final class UserServlet extends HttpServlet {
+  private static final String regex =
+      "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+  private static final Pattern pattern = Pattern.compile(regex);
   private ServletService service = new ServletService();
   private UserDAO persistence = new UserDAO();
 
@@ -43,21 +49,42 @@ public final class UserServlet extends HttpServlet {
 
       User user = new User();
 
-      user.setUsername(json.get("username").getAsString());
+      String username = json.get("username").getAsString();
 
-      user.setEmail(json.get("email").getAsString());
+      if (username.isEmpty() || username.length() > 32) {
+        throw new IllegalArgumentException("Username must be up to 32 characters length");
+      }
 
-      Bcrypt bcrypt = new Bcrypt();
+      String email = json.get("email").getAsString();
 
-      String password = bcrypt.encrypt(json.get("password").getAsString());
+      if (!pattern.matcher(email).matches()) {
+        throw new IllegalArgumentException("Invalid email address");
+      }
+
+      String password = json.get("password").getAsString();
+
+      if (password.length() < 8) {
+        throw new IllegalArgumentException("Password must have at least 8 characters");
+      }
+
+      user.setUsername(username);
+
+      user.setEmail(email);
+
+      Cipher cipher = new Bcrypt();
+
+      user.setPassword(cipher.encrypt(password));
 
       user.setPassword(password);
 
       if (!persistence.save(user)) {
-        throw new Exception("Err: " + user.toString());
+        throw new Exception(
+            "Username '" + username + "' or email '" + email + "' are already registered");
       }
 
-      service.dispatch(response, new Payload<User>(HttpStatus.CREATED, user));
+      service.dispatch(response, new Payload<>(HttpStatus.CREATED, user));
+    } catch (IllegalArgumentException exception) {
+      service.dispatch(response, new Payload<>(HttpStatus.BAD_REQUEST, exception.getMessage()));
     } catch (Exception exception) {
       service.dispatch(response, exception);
     }
@@ -76,12 +103,14 @@ public final class UserServlet extends HttpServlet {
       user.setID(id);
 
       if (!persistence.remove(user)) {
-        throw new Exception("Cannot delete user");
+        throw new Exception("User identified by " + id.toString() + " does not exists");
       }
 
-      service.dispatch(response, new Payload<>(HttpStatus.OK, "User deleted"));
+      service.dispatch(
+          response,
+          new Payload<>(HttpStatus.OK, "User identified by " + id.toString() + " deleted"));
     } catch (Exception exception) {
-      service.dispatch(response, exception);
+      service.dispatch(response, new Payload<>(HttpStatus.BAD_REQUEST, exception.getMessage()));
     }
   }
 }
